a12fa5d2afb8b5bdda9dd9e85d5e209887f4871a
ABDELWAHAB KHALIL <khalil.tadla@gmail.com>
2021-01-10 12:40:20 +0100

    commit

--- electrotadla/settings.py
+++ electrotadla/settings.py
@@ -20,15 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_hwu*+%rn624^*&a2yw9#kcbkd4f@99pm$hoasv5rm7$vywz8o'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if os.environ.get('ENV') == 'PRODUCTION':
-    DEBUG = False
-else:
-    DEBUG = True
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
